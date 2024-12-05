232b890498dbe16fa5278bfc92bcc9dc6891af8f
hieubk2502 <diemdungodau2502@gmail.com>
2024-12-06 00:36:38 +0700

    Add service Notification

---
+++ notification-service/src/main/java/com/dev/notification/controller/EmailController.java
@@ -0,0 +1,20 @@
+package com.dev.notification.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailController {
+
+    @PostMapping("/create")
+    void sendEmail(){
+        log.info("Call API success");
+    }
+}
